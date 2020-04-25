68b58ba10129985ab316f60332deea069b52d317
Mourjo Sen <mourjo@helpshift.com>
2020-04-25 15:09:24 +0530

    Handle exceptions in the scraper

--- src/lganguly/scrape.clj
+++ src/lganguly/scrape.clj
@@ -1,5 +1,6 @@
 (ns lganguly.scrape
   (:require [clj-http.client :as http]
+            [clojure.tools.logging :as log]
             [net.cgrand.enlive-html :as html]))
 
 (defn url->title
@@ -28,12 +29,18 @@
   " (url->title-html \"https://blog.ndk.io/clojure-compilation.html\")
    ;; => How to get HTTPS working on your local development environment in 5 minutes <a href=\"https://www.freecodecamp.org/news/how-to-get-https-working-on-your-local-development-environment-in-5-minutes-7af615770eec/amp/\" target=\"_blank\">https://www.freecodecamp.org/news/how-to-get-https-working-on-your-local-development-environment-in-5-minutes-7af615770eec/amp/</a>"
   [url]
-  (let [title (url->title url)]
-    (format "%s <a href=\"%s\" target=\"_blank\">%s</a>"
-            title
-            url
-            url)))
+  (try
+    (let [title (url->title url)]
+      (format "%s <a href=\"%s\" target=\"_blank\">%s</a>"
+              title
+              url
+              url))
+    (catch Exception e (log/error "Error in" url))))
+
 
 (defn url->title-html-main
   [& urls]
-  (doseq [url urls] (println (url->title-html url))))
+  (println)
+  (doseq [url urls]
+    (println (url->title-html url))
+    (println)))
